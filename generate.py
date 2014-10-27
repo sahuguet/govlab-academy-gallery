ed8fc611ca25b2f034e5503019a046e08d290b69
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2014-10-27 15:47:07 -0400

    support for all_projects.html

--- generate.py
+++ generate.py
@@ -30,6 +30,7 @@ def Main():
   FIELDS = ['name', 'project_title', 'team_name',
   'truonex_link', 'problem', 'why_compelling', 'summary',
   'tags', 'status', 'affiliation', 'country', 'class_of', 'online']
+  all_projects = []
   with open('projects.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -37,9 +38,16 @@ def Main():
       for i, item in enumerate(FIELDS):
         template_data[item] = row[i]
       html = template.render(template_data)
-      with open('gen/%s.html' % createUniqueName(template_data['project_title']), 'w') as g:
+      projectUniqueName = createUniqueName(template_data['project_title'])
+      with open('gen/%s.html' % projectUniqueName, 'w') as g:
         g.write(html)
         g.close()
-
+      all_projects.append({ 'name': template_data['project_title'], 'link': "%s.html" % projectUniqueName})
+    f.close()
+  template = env.get_template('all_projects.html')
+  print >> sys.stderr, len(all_projects)
+  with open('gen/all_projects.html', 'w') as g:
+    g.write(template.render({'projects': all_projects}))
+    g.close()
 if __name__ == '__main__':
   Main()
