262edcb3aa7c0e7948087d6c04a89e7d4c71817a
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2014-12-09 20:10:15 -0500

    adding people data + tiles

--- generate.py
+++ generate.py
@@ -24,9 +24,10 @@ def createUniqueName(title):
 def Main():
   env = Environment(loader=FileSystemLoader(TEMPLATES_DIR),
       extensions=['jinja2.ext.with_'])
+  env.filters['skolem'] = lambda x: x.lower().replace(' ', '_').replace('"','')
 
 
-  for page in ['about', 'offerings', 'index', 'resources']:
+  for page in ['offerings', 'index', 'resources']:
     template = env.get_template('%s.html' % page)
     html = template.render()
     with open('gen/%s.html' % page, 'w') as f:
@@ -36,10 +37,18 @@ def Main():
   # START Gallery 
   template = env.get_template('project.html')
 
+  TOPICS = [ 'topic%d' % k for k in range(1, 10)]
+  TYPES  = [ 'type%d' % k for k in range(1, 10)]
+  DIMS   = [ 'dim%d' % k for k in range(1, 10)]
+
+
+  import random
+
   FIELDS = ['name', 'project_title', 'team_name',
   'truonex_link', 'problem', 'why_compelling', 'summary',
   'tags', 'status', 'affiliation', 'country', 'class_of', 'online']
   all_projects = []
+  count = 0
   with open('projects.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -48,6 +57,11 @@ def Main():
         template_data[item] = row[i]
       if template_data['project_title'] == "":
         continue
+      template_data['id'] = count
+      count = count + 1
+      template_data['topic'] = random.choice(TOPICS)
+      template_data['type'] = random.choice(TYPES)
+      template_data['dim'] = random.choice(DIMS)
       html = template.render(template_data)
       projectUniqueName = createUniqueName(template_data['project_title'])
       with open('gen/%s.html' % projectUniqueName, 'w') as g:
@@ -60,9 +74,26 @@ def Main():
   template = env.get_template('all_projects.html')
   print >> sys.stderr, len(all_projects)
   with open('gen/gallery.html', 'w') as g:
-    g.write(template.render({'projects': all_projects}))
+    g.write(template.render({'projects': all_projects}, topics=TOPICS, types=TYPES, dims=DIMS))
     g.close()
   # Gallery END
+
+  # Doing the /about page
+  template = env.get_template('about.html')
+  with open('people.csv', 'rb') as f:
+    reader = csv.reader(f, delimiter='|')
+    people = []
+    all_clusters = set()
+    for row in reader:
+      (name, clusters) = row
+      clusters = map(lambda x:x.strip(), clusters.split(','))
+      for c in clusters: all_clusters.add(c)
+      people.append({'name': name, 'clusters': clusters})
+    f.close()
+    people = sorted(people, key=lambda x:x['name'])
+  with open('gen/about.html', 'w') as g:
+    g.write(template.render({ 'people': people, 'all_clusters': all_clusters }))
+    g.close()
 
 if __name__ == '__main__':
   Main()
